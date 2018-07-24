9d1e78895197b7c38938af5117867df5f5527590
Denis Gibikoski <d3nys.inf@gmail.com>
2018-07-24 16:37:16 -0300

    ItemPedido e ItemPedido PK

--- src/main/java/com/denis/cursosmc/domain/Produto.java
+++ src/main/java/com/denis/cursosmc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.denis.cursosmc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,42 +13,33 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto implements Serializable{
+public class Produto implements Serializable {
 
-	
-	
-	
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	
+
 	private String nome;
-	
-	
+
 	private Double preco;
-	
+
 	@JsonBackReference
 	@ManyToMany
-	@JoinTable( name = "PRODUTO_CATEGORIA", 
-	joinColumns = @JoinColumn(name= "produto_id"),
-	inverseJoinColumns = @JoinColumn (name="categoria_id")	
-	)
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
-
-
 	
-
+	@OneToMany(mappedBy= "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
-		
-	}
 
+	}
 
 	public Produto(Integer id, String nome, Double preco) {
 		super();
@@ -54,47 +47,55 @@ public class Produto implements Serializable{
 		this.nome = nome;
 		this.preco = preco;
 	}
-
+	
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
 
 	public Integer getId() {
 		return id;
 	}
 
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public Double getPreco() {
 		return preco;
 	}
 
-
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
 
 	public List<Categoria> getCategorias() {
 		return categorias;
 	}
 
-
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -103,7 +104,6 @@ public class Produto implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -122,12 +122,9 @@ public class Produto implements Serializable{
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Produto [id=" + id + ", nome=" + nome + ", preco=" + preco + ", categorias=" + categorias + "]";
 	}
-	
-	
-	
+
 }
