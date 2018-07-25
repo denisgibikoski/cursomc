10bd0a48aab421eeb07dc81700bc8505367d2d36
Denis Gibikoski <d3nys.inf@gmail.com>
2018-07-25 14:40:37 -0300

    Atualizando JsonManger por JsonIgnore

--- src/main/java/com/denis/cursosmc/domain/PagamentoComBoleto.java
+++ src/main/java/com/denis/cursosmc/domain/PagamentoComBoleto.java
@@ -6,48 +6,41 @@ import javax.persistence.Entity;
 
 import com.denis.cursosmc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
-	@JsonFormat(pattern ="dd/MM/yyyy")
-		private Date dataVencimento;
-	@JsonFormat(pattern ="dd/MM/yyyy")
-		private Date dataPagamento;
-		
-		
-		public PagamentoComBoleto() {
-			
-		}
 
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataVencimento;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataPagamento;
 
-		public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido,
-				Date dataVencimento, Date dataPagamento) {
-			super(id, estado, pedido);
-			this.dataPagamento = dataPagamento;
-			this.dataVencimento = dataVencimento;
-		}
+	public PagamentoComBoleto() {
 
+	}
 
-		public Date getDataVencimento() {
-			return dataVencimento;
-		}
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
 
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
 
-		public void setDataVencimento(Date dataVencimento) {
-			this.dataVencimento = dataVencimento;
-		}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
 
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
 
-		public Date getDataPagamento() {
-			return dataPagamento;
-		}
-
-
-		public void setDataPagamento(Date dataPagamento) {
-			this.dataPagamento = dataPagamento;
-		}
-		
-		
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
 
 }
